2c48cb2e9c9f450268b88547c660731fb268f7e1
Denman Rooke <denman@digitgaming.com>
2015-12-21 23:06:56 +0000

    Implementing drag & drop unit hand

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    Transform ParentToReturnTo = null;
+
+	public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+
+        ParentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(ParentToReturnTo);
+    }
+}
