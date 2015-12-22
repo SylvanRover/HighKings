e9722afd033d0d1a2aac4695f522db65c03091f5
davidkellydigit <davidkelly@DK-iMac.local>
2015-12-22 17:56:58 +0000

    Networking

--- Assets/Scripts/Dropzone.cs
+++ Assets/Scripts/Dropzone.cs
@@ -3,15 +3,23 @@ using System.Collections;
 using UnityEngine.EventSystems;
 
 public class Dropzone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
-
+	/*
     public bool isCapturePoint = false;
     public bool isOwned = false;
     public bool isAvailable = true;
+    */
     private PlayerController playerController;
     private UnitCardStats unitCardStats;
     private MakePrefabAppear spawn;
     
+	private SimpleStatus.DropZoneState _state;
+
+
     void Start() {
+		_state = new SimpleStatus.DropZoneState ();
+		_state.isCapturePoint = true;
+		_state.playerOccupier = -1;
+		_state.owned = false;
         spawn = gameObject.GetComponent<MakePrefabAppear>();
         playerController = GameObject.FindGameObjectWithTag("PlayerController").GetComponent<PlayerController>();
 
@@ -35,12 +43,33 @@ public class Dropzone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
         unitCardStats = d.GetComponent<UnitCardStats>();
 
-        if (isCapturePoint && isOwned && isAvailable) {
-            if (playerController.GoldCurrent >= unitCardStats.unitCost) {
-                spawn.Appear();
+        //if (isCapturePoint && isOwned && isAvailable) {
+            //if (playerController.GoldCurrent >= unitCardStats.unitCost) {
+                
                 playerController.GoldCurrent = playerController.GoldCurrent - unitCardStats.unitCost;
-            }
-        }
+				_state.owned = false;	
+				_state.playerOccupier = SimpleNet.PlayerID;
+				spawn.Appear (); //FOR NOW
+           // }
+        //}
     }
+
+	public void UpdateState(SimpleStatus.DropZoneState state){
+		if (this._state.playerOccupier != state.playerOccupier || this._state.owned != state.owned) {
+			Debug.LogError ("STATE DOES NOT MATCH");
+			if (state.playerOccupier != -1) {
+				// THEN must have an occupier
+				spawn.Appear (); //FOR NOW
+			} else {
+				// THEN no longer has an occupier
+				//TODO: delete the occupier graphic
+			}
+		}
+		this._state = state;
+	}
+
+	public SimpleStatus.DropZoneState GetState(){
+		return this._state;
+	}
 
 }
